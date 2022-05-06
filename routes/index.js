600ebe1c60890143689fa2cc3d6b430db8ba465b
Erik <ewpeters@asu.edu>
2022-05-06 15:41:13 -0700

    move webhook code

--- routes/index.js
+++ routes/index.js
@@ -7,48 +7,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Expensi" });
 });
 
-/* E.g. /send?amt=8&desc=Parking%20at%20U%20of%20A&enfa=false */
-router.post('/send', function(req, res, next) {
-  console.log("Attempting API call...");
 
-  // var user_id = (req.query.enfa === "true" ? process.env.ERIK_ID : process.env.ENFA_ID);
-  console.log(req.body);
-  // sendApiCall(req.query.amt, req.query.desc, user_id);
-  
-  res.render('index', { title: "Expense  added", body: req.body });
-});
-
-function sendApiCall(amt, desc, user_id) {
-  const bearerStr = 'Bearer ' + process.env.PERSONAL_KEY;
-  const data = JSON.stringify({
-    cost:          amt,
-    description:   desc,
-    currency_code: 'USD',
-    group_id:      process.env.GROUP_ID,
-    split_equally: true
-  });
-  var options = {
-    hostname: 'secure.splitwise.com',
-    port: 443,
-    path: '/api/v3.0/create_expense/',
-    method: 'POST',
-    headers:{
-        'Authorization': bearerStr,
-        'Content-Type': 'application/json',
-        'Content-Length': data.length
-    }
-  };
-  const req = https.request(options, res => {
-    console.log(`statusCode: ${res.statusCode}`);
-    console.log(`statusMessage: ${res.statusMessage}`);
-  });
-
-  req.on('error', error => {
-    console.error(error);
-  });
-
-  req.write(data);
-  req.end();
-};
 
 module.exports = router;

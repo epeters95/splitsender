ec129950d60257c284b7640c176f6aeef5c2bd74
Erik <ewpeters@asu.edu>
2022-05-08 11:17:12 -0700

    fix notation

--- routes/users.js
+++ routes/users.js
@@ -51,10 +51,7 @@ const sendAccessTokenRequest = (authCode, userId) => {
   const req = https.request(options, (res) => {
     res.setEncoding('utf8');
     res.on('data', function (data) {
-      console.log('data is ..........');
-      console.log(data);
-      console.log(data.access_token);
-      db.createUserToken(data.access_token, userId);
+      db.createUserToken(data["access_token"], userId);
     });
   });
 
